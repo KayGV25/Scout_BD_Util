d2bfb6d5bc937fc654850298386908e71b4abc65
KayGV25 <khuongvudang25@gmail.com>
2026-05-09 16:13:28 +0700

    change space a1z26

--- src/components/CryptedOut.jsx
+++ src/components/CryptedOut.jsx
@@ -216,7 +216,7 @@ export default function CryptedOut({ cKey, value, type, plain }){
                 
         }
 
-        return res.join(" ");
+        return res.join("");
     }
     
     function findNearestSquareRoot(num){
